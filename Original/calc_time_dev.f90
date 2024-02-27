18991d7b036cf989914c6b03a5f871a1079df3da
htymjun <jhatayama@s.okayama-u.ac.jp>
2024-02-27 15:29:22 +0900

    MUSCL is TBA

--- Original/calc_time_dev.f90
+++ Original/calc_time_dev.f90
@@ -1,30 +1,10 @@
 module calc_time_dev
   implicit none
 contains
-  subroutine calc_quantities(nx,ny,nz,gamma,Cp,Q,rho,u,v,w,p,T)
-    integer, intent(in) :: nx, ny, nz
-    real(8), intent(in) :: gamma, Cp
-    real(8), intent(in), dimension(nx,ny,nz,5) :: Q
-    real(8), intent(out), dimension(nx,ny,nz) :: rho, u, v, w, p, T
-    integer i, j, k
-    do k = 1, nz
-      do j = 1, ny
-        do i = 1, nx
-          rho(i,j,k) = Q(i,j,k,1)
-          u(i,j,k) = Q(i,j,k,2) / rho(i,j,k)
-          v(i,j,k) = Q(i,j,k,3) / rho(i,j,k)
-          w(i,j,k) = Q(i,j,k,4) / rho(i,j,k)
-          p(i,j,k) = (gamma-1.d0)*(Q(i,j,k,5)-0.5d0*rho(i,j,k)*(u(i,j,k)**2+v(i,j,k)**2+w(i,j,k)**2))
-          T(i,j,k) = ((Q(i,j,k,5) + p(i,j,k)) / rho(i,j,k) &
-          & - 0.5d0 * (u(i,j,k) ** 2 + v(i,j,k) ** 2 + w(i,j,k) ** 2)) / Cp
-        enddo
-      enddo
-    enddo
-  end subroutine
-
   subroutine RungeKutta(nx,ny,nz,nt,np,dx,dy,dz,dt,gamma,mu,kappa,Cp,Cs,Q)
     use iso_fortran_env
     use mod_globals, only : accuracy
+    use calc_physical_quantities
     use calc_steps
     use calc_KEEP
     use calc_SLAU
