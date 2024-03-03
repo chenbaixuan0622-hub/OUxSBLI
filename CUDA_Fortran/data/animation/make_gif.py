9efa09b9c4aaffc721157fee0f210de519d8a559
htymjun <jhatayama@s.okayama-u.ac.jp>
2024-03-04 00:43:42 +0900

    git file was created

---
+++ CUDA_Fortran/data/animation/make_gif.py
@@ -0,0 +1,6 @@
+from PIL import Image
+import glob
+
+files = sorted(glob.glob('./*.png'))
+images = list(map(lambda file : Image.open(file), files))
+images[0].save('Taylor_Green.gif', save_all = True, append_images = images[1:], duration = 2000, loop = 0)
