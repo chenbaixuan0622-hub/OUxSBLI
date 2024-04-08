ee8d4f8c05b59c403d6d6499d6c5ec5623ddc544
htymjun <jhatayama@s.okayama-u.ac.jp>
2024-04-08 15:03:56 +0900

    4th-order MUSCL was added, but simulation divergede

---
+++ 2D_solver/oblique_shock_4th/mod_globals.f90
@@ -0,0 +1,89 @@
+module mod_globals
+  use cudafor
+  implicit none
+  integer, parameter :: dimension = 2
+  integer, parameter :: accuracy = 2 ! only 2nd-order accuracy is available
+  integer(kind=2**(accuracy/2)), parameter :: id_accuracy = 1
+  integer, parameter :: offset = accuracy / 2
+  integer, parameter :: id_visc = 0
+  !!!!!!!!!!!!!!!!!!!!!!!!!
+  ! id_hybrid ! kind2 off !
+  !           ! kind4 on  !
+  !!!!!!!!!!!!!!!!!!!!!!!!!
+  ! id_muscl  ! kind2 off !
+  !           ! kind4 3rd !
+  !           ! kind8 4th !
+  !!!!!!!!!!!!!!!!!!!!!!!!!
+  ! id_scheme ! 1  KEEP   !
+  !           ! 2  Roe    !
+  !           ! 3  SLAU   !
+  !!!!!!!!!!!!!!!!!!!!!!!!!
+  integer(kind=2), parameter :: id_hybrid = 0.d0
+  integer(kind=4), parameter :: id_muscl = 0.d0
+  integer, parameter :: id_scheme = 3
+
+  ! mesh
+  real(8), parameter :: Lx = 48d-3
+  real(8), parameter :: Ly = 16d-3
+  real(8), parameter :: Lz = 2d-3
+  integer, parameter :: nx = 513
+  integer, parameter :: ny = 321
+  integer, parameter :: nz = accuracy+1
+  real(8), parameter :: dx = Lx / (nx-1)
+  real(8), parameter :: dy = Ly / (ny-1)
+  real(8), parameter :: dz = Lz / (nz-1)
+  real(8), parameter :: dxi = 1.d0 / dx
+  real(8), parameter :: dyi = 1.d0 / dy
+
+  ! GPU
+  type(dim3) :: blocksE = dim3((nx-accuracy+1)/32,(ny-accuracy)/11,1)
+  type(dim3) :: blocksF = dim3((nx-accuracy)/7,(ny-accuracy+1)/32,1)
+  type(dim3) :: threadsE = dim3(32,11,1)
+  type(dim3) :: threadsF = dim3(7,32,1)
+
+  ! time
+  integer, parameter :: nt = 50
+  integer, parameter :: np = 40
+  real(8), parameter :: u0 = 506.8d0
+  real(8), parameter :: dt = 0.5d0 * dx/ u0
+
+  real(8), parameter :: dtdx = dt / dx
+  real(8), parameter :: dtdy = dt / dy
+  real(8), parameter :: dtdz = 0.d0
+
+  ! physical properties
+  real(8), parameter :: gamma = 1.4d0
+
+  ! MUSCL
+  real(8), parameter :: k = 1.d0 / 3.d0
+  real(8), parameter :: b = (3.d0 - k) / (1.d0 - k)
+  real(8), parameter :: omega = 4.d0
+  real(8), parameter :: sigma = 2.d0
+  real(8), parameter :: eps = 1.d0
+
+  ! oblique shock
+  real(8), parameter :: R = 287.03d0
+  real(8), parameter :: M0 = 1.9d0
+  real(8), parameter :: p0 = 14924.d0
+  real(8), parameter :: T = 171.31d0
+  real(8), parameter :: beta = dacos(-1.d0) * 37.2d0 / 180.d0
+  real(8), parameter :: Ms = M0 * dsin(beta)
+  real(8), parameter :: Ms2 = Ms**2
+  real(8), parameter :: theta = datan(2.d0 * (1.d0 / dtan(beta)) * (Ms2 - 1.d0) / (M0**2 * (gamma + dcos(2.d0 * beta)) + 2.d0))
+  real(8), parameter :: rho0 = p0 / (R * T)
+  real(8), parameter :: rho2 = rho0 * (gamma + 1.d0) * Ms2 / ((gamma - 1.d0) / (M0**2 * (gamma + dcos(2.d0 * beta)) + 2.d0))
+  real(8), parameter :: p2 = p0 * (1.d0 + 2.d0 * gamma * (Ms2 - 1.d0) / (gamma + 1.d0)) 
+  real(8), parameter :: u1 = u0 * dsin(beta)
+  real(8), parameter :: v1 = u0 * dcos(beta)
+  real(8), parameter :: a1 = u0 / M0
+  real(8), parameter :: u2 = u1 - 2.d0 * a1 * (Ms - 1.d0 / Ms) / (gamma + 1.d0)
+  real(8), parameter :: v2 = u0 * dcos(beta)
+  real(8), parameter :: u_magnitude = sqrt(u2**2 + v2**2)
+  real(8), parameter :: ux = u_magnitude * dcos(theta)
+  real(8), parameter :: uy = -u_magnitude * dsin(theta)
+
+  ! variables
+  real(8), save :: Q(nx,ny,4)
+  real(8), save :: T0(nx,ny)
+end module mod_globals
+
--- src/calc_muscl.f90
+++ src/calc_muscl.f90
@@ -1,39 +1,107 @@
 module calc_muscl
   implicit none
+  interface minmod
+    module procedure minmod2, minmod3
+  end interface
+
+  interface al
+    module procedure al_3rd, al_4th
+  end interface
+
+  interface ar
+    module procedure ar_3rd, ar_4th
+  end interface
 contains
-  attributes(device) function minmod(x,y) result(ans)
+
+!minmod!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  attributes(device) function minmod2(x,y) result(ans)
     real(8), intent(in), value :: x, y
     real(8) :: ans, sgn
     sgn = dsign(1.d0, x)
     ans = sgn * max(min(abs(x), sgn * y), 0.d0)
-  end function minmod
+  end function minmod2
 
-  attributes(device) function al(a,eps,k,b,d_p,d_m) result(ans)
+  attributes(device) function minmod3(x,y,z) result(ans)
+    real(8), intent(in), value :: x, y, z
+    real(8) :: ans, sgn
+    sgn = dsign(1.d0, x)
+    ans = sgn * max(min(abs(x), sgn * y, sgn * z), 0.d0)
+  end function minmod3
+
+!3rd-order!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  attributes(device) function al_3rd(a,eps,k,b,d_p,d_m) result(ans)
     real(8), intent(in), value :: a, eps, k, b, d_p, d_m
     real(8) :: ans, delta_p, delta_m
     delta_p = minmod(d_p, b * d_m)
     delta_m = minmod(d_m, b * d_p)
     ans = a + 0.25d0 * eps * ((1.d0 - k) * delta_m + (1.d0 + k) * delta_p)
-  end function al
+  end function al_3rd
 
-  attributes(device) function ar(a,eps,k,b,d_p,d_m) result(ans)
+  attributes(device) function ar_3rd(a,eps,k,b,d_p,d_m) result(ans)
     real(8), intent(in), value :: a, eps, k, b, d_p, d_m
     real(8) :: ans, delta_p, delta_m
     delta_p = minmod(d_p, b * d_m)
     delta_m = minmod(d_m, b * d_p)
     ans = a - 0.25d0 * eps * ((1.d0 - k) * delta_p + (1.d0 + k) * delta_m)
-  end function ar
+  end function ar_3rd
 
-  attributes(device) subroutine MUSCL(dim,eps,k,b,Q1,Q2,d1,d2,d3,Ql,Qr)
+!4th-order!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  attributes(device) function d33(sigma,d1,d2,d3) result(ans)
+    real(8), intent(in), value :: sigma, d1, d2, d3 
+    real(8) :: ans, da, db, dc
+    da = minmod(d1, sigma * d2, sigma * d3)
+    db = minmod(d2, sigma * d1, sigma * d3)
+    dc = minmod(d3, sigma * d1, sigma * d2)
+    ans = da - 2.d0 * db + dc
+  end function d33
+
+  attributes(device) function al_4th(a,eps,w,sigma,d1,d2,d3,d4) result(ans)
+    real(8), intent(in), value :: a, eps, w, sigma, d1, d2, d3, d4
+    real(8) :: ans, delta_p, delta_m, dp, dm
+    dp = d3 - eps * d33(sigma, d2, d3, d4) / 6.d0
+    dm = d2 - eps * d33(sigma, d1, d2, d3) / 6.d0
+    delta_p = minmod(dp, w * dm)
+    delta_m = minmod(dm, w * dp)
+    ans = a + (delta_m + 2.d0 * delta_p) / 6.d0
+  end function al_4th
+
+  attributes(device) function ar_4th(a,eps,w,sigma,d1,d2,d3,d4) result(ans)
+    real(8), intent(in), value :: a, eps, w, sigma, d1, d2, d3, d4
+    real(8) :: ans, delta_p, delta_m, dp, dm
+    dp = d3 - eps * d33(sigma, d2, d3, d4) / 6.d0
+    dm = d2 - eps * d33(sigma, d1, d2, d3) / 6.d0
+    delta_p = minmod(dp, w * dm)
+    delta_m = minmod(dm, w * dp)
+    ans = a - (delta_p + 2.d0 * delta_m) / 6.d0
+  end function ar_4th
+
+!MUSCL interpolation!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  attributes(device) subroutine MUSCL(dim,eps_left,eps_right,k,b,Q1,Q2,d1,d2,d3,Ql,Qr)
     integer, intent(in), value :: dim
-    real(8), intent(in), value :: eps, k, b
+    real(8), intent(in), value :: eps_left, eps_right, k, b
     real(8), intent(in), dimension(dim), device :: Q1, Q2, d1, d2, d3
     real(8), intent(out), dimension(dim), device :: Ql, Qr
     integer i
     do i = 1, dim
-      Ql(i) = al(Q1(i), eps, k, b, d2(i), d1(i))
-      Qr(i) = ar(Q2(i), eps, k, b, d3(i), d2(i))
+      Ql(i) = al(Q1(i), eps_left, k, b, d2(i), d1(i))
+      Qr(i) = ar(Q2(i), eps_right, k, b, d3(i), d2(i))
     enddo
   end subroutine MUSCL
+
+  attributes(device) subroutine MUSCL_4th(dim,eps_left,eps_right,w,sigma,Q1,Q2,d1,d2,d3,d4,d5,Ql,Qr)
+    integer, intent(in), value :: dim
+    real(8), intent(in), value :: eps_left, eps_right, w, sigma
+    real(8), intent(in), dimension(dim), device :: Q1, Q2, d1, d2, d3, d4, d5
+    real(8), intent(out), dimension(dim), device :: Ql, Qr
+    integer i
+    do i = 1, dim
+      Ql(i) = al(Q1(i), eps_left, w, sigma, d4(i), d3(i), d2(i), d1(i))
+      Qr(i) = ar(Q2(i), eps_right, w, sigma, d5(i), d4(i), d3(i), d2(i))
+    enddo
+  end subroutine MUSCL_4th
 end module calc_muscl
 
