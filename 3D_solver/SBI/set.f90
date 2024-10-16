005369d3b1d77e04aa68c041aff01446ca5f929d
htymjun <jhatayama@s.okayama-u.ac.jp>
2024-10-16 09:18:52 +0900

    initial conditions for TBL and SBI were modified. And, rescaling method worked well

--- 3D_solver/SBI/set.f90
+++ 3D_solver/SBI/set.f90
@@ -2,42 +2,6 @@ module set
   use mod_globals, only : id_rescale, nx, ny, nz, nre, Lx, Ly, Lz, gamma, R, rho0, rho2, u0, ux, uy, p0, p2, T0, M0
   implicit none
 contains
-  subroutine calc_Blasius(eta,d,u,v)
-    real(8), intent(in), value  :: eta, d
-    real(8), intent(out)        :: u, v
-    real(8) f, df, x
-    real(8) fs(45), dfs(45)
-    real(8) :: nu0
-    integer i
-    nu0 = (1.716d-5 * ((273.2d0 + 111.d0) / (T0 + 111.d0)) * (T0 / 273.2d0)**1.5d0) / rho0 
-    fs(:) = (/0.d0, 0.00664d0, 0.02656d0, 0.05974d0, 0.10611d0, 0.16557d0, 0.23795d0, &
-    & 0.32298d0, 0.42032d0, 0.52952d0, 0.65003d0, 0.78120d0, 0.92230d0, 1.07252d0, &
-    & 1.23099d0, 1.39682d0, 1.56911d0, 1.74696d0, 1.92954d0, 2.11605d0, 2.30576d0, &
-    & 2.49806d0, 2.69238d0, 2.88826d0, 3.08534d0, 3.28329d0, 3.48189d0, 3.68094d0, &
-    & 3.88031d0, 4.07990d0, 4.27964d0, 4.47948d0, 4.67938d0, 4.87931d0, 5.07928d0, &
-    & 5.27926d0, 5.47925d0, 5.67924d0, 5.87924d0, 6.07923d0, 6.27923d0, 6.47923d0, &
-    & 6.67923d0, 6.87923d0, 7.07923d0/)
-    dfs(:) = (/0.d0, 0.06641d0, 0.13277d0, 0.19894d0, 0.26471d0, 0.32979d0, 0.39378d0, &
-    & 0.45627d0, 0.51676d0, 0.57477d0, 0.62977d0, 0.68132d0, 0.72899d0, 0.77246d0, &
-    & 0.81152d0, 0.84605d0, 0.87609d0, 0.90177d0, 0.92333d0, 0.94112d0, 0.95552d0, &
-    & 0.96696d0, 0.97587d0, 0.98269d0, 0.98779d0, 0.99155d0, 0.99425d0, 0.99616d0, &
-    & 0.99748d0, 0.99838d0, 0.99898d0, 0.99937d0, 0.99961d0, 0.99977d0, 0.99987d0, &
-    & 0.99992d0, 0.99996d0, 0.99998d0, 0.99999d0, 1.00000d0, 1.00000d0, 1.00000d0, &
-    & 1.00000d0, 1.00000d0, 1.00000d0/)
-    do i = 1, 44
-      x = 0.2d0 * dble(i-1) 
-      if (x <= eta .and. eta <= x + 0.2d0) then
-        f = fs(i) + 5.d0 * (fs(i+1) - fs(i)) * (eta - x)
-        df = dfs(i) + 5.d0 * (dfs(i+1) - dfs(i)) * (eta - x)
-      elseif (8.8d0 <= eta) then
-        f = 7.07923d0
-        df = 1.d0
-      endif
-    enddo
-    u = u0 * df
-    v = 0.d0!0.5d0 * (nu0 / d) * (min(eta,8.8d0) * df - f)
-  end subroutine calc_Blasius
-
   subroutine set_grid(nx,ny,nz,x,y,z,dx,dy,dz)
     integer, intent(in)   :: nx, ny, nz
     real(8), intent(out)  :: x(nx), y(ny), z(nz), dx(nx-1), dy(ny-1), dz(nz-1)
@@ -73,29 +37,62 @@ contains
     real(8), intent(in)                         :: xs(nx), ys(ny), zs(nz)
     real(8), intent(out), dimension(nx,ny,nz,5) :: Q
     integer i, j, k, No
-    real(8) :: d = 0.2d0 * 1.d-3
-    real(8) :: d1= 2.d-3
-    real(8) :: eta, rho, u, v, w, T, p_wall
+    real(8) :: blt0 = 0.5d0 * blt
+    real(8) :: Cp   = gamma * R / (gamma - 1.d0)
+    real(8) :: eta, rho, u, v, w, T, Tw, Taw, p_wall
     ! random
-    real(8) :: std, ustd, Tstd
+    real(8) :: std, ustd, vstd, wstd, Tstd
+    real(8), allocatable :: randum(:,:,:,:)
+    integer ir, jr, kr, nxr, nyr, nzr
+
     No = int(0.5d0 * nx)
+
+    ! generate randum
+    nxr = (nx+9) / 10
+    nyr = (ny+1) / 2
+    nzr = (nz+1) / 2
+    allocate(randum(nxr,nyr,nzr,4))
+
+    do k = 1, nzr
+      do j = 1, nyr
+        do i = 1, nxr
+          call random_number(randum(i,j,k,1))
+          call random_number(randum(i,j,k,2))
+          call random_number(randum(i,j,k,3))
+          call random_number(randum(i,j,k,4))
+          randum(i,j,k,1) = 2.d0 * randum(i,j,k,1) - 1.d0
+          randum(i,j,k,2) = 2.d0 * randum(i,j,k,2) - 1.d0
+          randum(i,j,k,3) = 2.d0 * randum(i,j,k,3) - 1.d0
+          randum(i,j,k,4) = 2.d0 * randum(i,j,k,4) - 1.d0
+    enddo;enddo;enddo
+
     do k = 1, nz
       do j = 1, ny
         do i = 1, nx
-          eta = ys(j) / d
-          call calc_Blasius(eta,d,u,v)
-          if (ys(j) <= d1) then
-            call random_number(std)   ! 0 <= std <= 1
-            std = 2.d0 * std - 1.d0   !-1 <= std <= 1
+          eta = 5.d0 * ys(j) / blt0
+          u   = min(u0, u0 * (0.0015d0 * eta**4 - 0.0181d0 * eta**3 + 0.029d0 * eta**2 + 0.3192 * eta + 0.0003d0))
+          v   = 0.d0
+          Taw  = T0 + 0.5d0 * u0**2 / Cp
+          Tw   = Taw
+          T    = Tw + (Taw - Tw) * u / u0 - 0.5d0 * Pr**(1.d0/3.d0) * u**2 / Cp
+          if (10 < j .and. ys(j) <= blt) then
+            ir = i / 10 + 1
+            jr = j / 2  + 1
+            kr = k / 2  + 1
+            ustd = 0.2d0 * u0 * randum(ir,jr,kr,1)
+            vstd = 0.1d0 * u0 * randum(ir,jr,kr,2)
+            wstd = 0.1d0 * u0 * randum(ir,jr,kr,3)
+            Tstd = T0 * (gamma - 1.d0) * M0**2 * 0.2d0 * randum(ir,jr,kr,4)
           else
-            std = 0.d0
+            ustd = 0.d0
+            vstd = 0.d0
+            wstd = 0.d0
+            Tstd = 0.d0
           endif
-          ustd = 0.1d0 * u * std
-          u = u + ustd
-          v = v + 0.5d0 * ustd
-          w = 0.5d0 * ustd
-          Tstd = T0 * (gamma - 1.d0) * M0**2 / u0
-          T = T0 + Tstd * std
+          u   = u + ustd
+          v   = v + vstd
+          w   = wstd
+          T   = T + Tstd
           rho = p0 / (R * T)
           Q(i,j,k,1) = rho
           Q(i,j,k,2) = Q(i,j,k,1) * u
@@ -103,6 +100,8 @@ contains
           Q(i,j,k,4) = Q(i,j,k,1) * w
           Q(i,j,k,5) = p0 / (gamma - 1.d0) + 0.5d0 * (Q(i,j,k,2)**2 + Q(i,j,k,3)**2 + Q(i,j,k,4)**2) / Q(i,j,k,1)
     enddo;enddo;enddo
+
+    deallocate(randum)
 
     !do k = 1, nz
     !  do i = No, nx
