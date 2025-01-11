12b21487e617d08c2f8c6b6539da09f747020a59
htymjun <jhatayama@s.okayama-u.ac.jp>
2025-01-12 00:04:55 +0900

    fast 3D Poisson solver was implemented

--- post/calc_POD3d.py
+++ post/calc_POD3d.py
@@ -6,8 +6,8 @@ from matplotlib.animation import FuncAnimation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from tqdm import tqdm
 from mod.mod_plot import set_Params
-from mod.mod_read import getGrid, getVector, extract_number
-from mod.mod_POD import make_data3D, snapshot_pod, calc_time_coef
+from mod.mod_read import getGrid, getVector, extract_number, Data
+from mod.mod_POD import make_data3D, make_grid3D, snapshot_pod, calc_time_coef
 from mod.mod_plot import print_VTK
 
 
@@ -15,22 +15,22 @@ set_Params()
 
 # parameter
 #Q_dir   = "../3D_solver/TBL/data"#"../../z6mm"
-Q_dir   = "../../../../../../media/user/HD-EDS-E/TBL/SBLI_1delta"
+Q_dir   = "../../../../../../media/user/HD-EDS-E/TBL/SBLI_1delta/HD"
 Lx1     = 24.e-3
 Lx2     = 40.e-3
 Ly1     = 0.e-3
-Ly2     = 3.e-3
+Ly2     = 10.e-3
 Lz1     = 0.e-3 
 Lz2     = 2.e-3
-stridex = 4
-stridey = 8
-stridez = stridex * 2
-endT    = 0.1e-3
+stridex = 1
+stridey = 1
+stridez = 1
+endT    = 0.8e-3
 
 
-def plot_pod_results(POD_dir, x, y, z, data, eigenvalues, modes, time_coefficients, num_modes=6):
+def plot_pod_results(POD_dir, x, y, z, t, data, eigenvalues, modes, time_coefficients, num_modes):
   # energy
-  plt.figure(figsize=(6, 4))
+  plt.figure(figsize=(4, 4))
   plt.plot(range(1,11), eigenvalues[:10] / np.sum(eigenvalues) * 100, 'o-')
   plt.title('Energy Contribution of Modes')
   plt.xlabel('Mode Index')
@@ -51,41 +51,63 @@ def plot_pod_results(POD_dir, x, y, z, data, eigenvalues, modes, time_coefficien
     print_VTK(x, y, z, u, POD_dir, name)
 
   # time coefficient
-  fig, ax = plt.subplots(num_modes, 1, figsize=(8, 8))
+  row = 2
+  col = num_modes//2
+  fig, ax = plt.subplots(col, row, figsize=(12, 8))
   for i in range(num_modes):
-    ax[i].plot(time_coefficients[:,i])
-    
+    ax[i%col,i//col].plot(t*1e3, time_coefficients[:,i])
+    ax[i%col,i//col].set_xlabel("t [ms]")
+    ax[i%col,i//col].set_xlim(t[0]*1e3, t[-1]*1e3)
+    ax[i%col,i//col].set_ylabel(f'Mode {i+1}')
+
   save_name = "POD_time_coef"
   save_path = os.path.join(POD_dir, save_name)
   np.save(save_path, time_coefficients)
 
+  fig.tight_layout()
   save_name = "POD_time_coef.png"
   save_path = os.path.join(POD_dir, save_name)
   plt.savefig(save_path)
   plt.close()
 
 
-def plot_reconstruction(POD_dir, x, y, z, mean, data, modes, time_coef, num_modes):
+def plot_reconstruction(POD_dir, x, y, z, modes, time_coef, num_modes, mean=None):
   Nt = len(time_coef[:,0])
   nx = len(x)
   ny = len(y)
   nz = len(z)
   POD = np.dot(time_coef[:,:num_modes], modes[:,:num_modes].T)
+  if mean is not None:
+    POD = POD + mean[None,:]
   POD = np.reshape(POD, [Nt,nz,ny,nx])
   for i in range(Nt):
     name = "POD" + str(i+1).zfill(5)
     print_VTK(x, y, z, POD[i,:,:,:], POD_dir, name)
 
 
-POD_dir = os.path.join(Q_dir, "POD3D")
-os.makedirs(POD_dir, exist_ok=True)
+def main(Q_dir, Lx1, Lx2, Ly1, Ly2, Lz1, Lz2, stridex, stridey, stridez, endT):
+  POD_dir = os.path.join(Q_dir, "vector")
+  os.makedirs(POD_dir, exist_ok=True)
+  D_path = os.path.join(POD_dir, "D.npy")
 
-x, y, z, t, D = make_data3D(Lx1, Lx2, Ly1, Ly2, Lz1, Lz2, stridex, stridey, stridez, endT, Q_dir)
-mean = np.mean(D)
-D    = D - mean
-eigenvalues, eigenvectors, modes = snapshot_pod(D)
-time_coef = calc_time_coef(D, modes)
+  if os.path.isfile(D_path):
+    data = Data(Q_dir, endT, Lx1=None, Lx2=None, Ly1=None, Ly2=None, Lz1=None, Lz2=None)
+    _, _, _, _, _, _, x, y, z, t = data.make_grid()
+    D = np.load(D_path)
+  else:
+    # D[space, time]
+    x, y, z, t, D = make_data3D(Q_dir, endT, Lx1, Lx2, Ly1, Ly2, Lz1, Lz2, stridex, stridey, stridez)
+    np.save(D_path, D)
+    
+  mean = np.mean(D[:,120:], axis=-1)
+  D[:,120:] = D[:,120:] - mean[:,None]
 
-plot_pod_results(POD_dir, x, y, z, D, eigenvalues, modes, time_coef, num_modes=6)
-plot_reconstruction(POD_dir, x, y, z, mean, D, modes, time_coef, num_modes=6)
+  eigenvalues, eigenvectors, modes = snapshot_pod(D[:,120:])
+  time_coef = calc_time_coef(D[:,120:], modes)
+
+  plot_pod_results(POD_dir, x, y, z, t[:-120], D[:,120:], eigenvalues, modes, time_coef, num_modes=10)
+  #plot_reconstruction(POD_dir, x, y, z, modes, time_coef, num_modes=10, mean=mean)
+
+
+main(Q_dir, Lx1, Lx2, Ly1, Ly2, Lz1, Lz2, stridex, stridey, stridez, endT)
 
--- post/mod/mod_Poisson.py
+++ post/mod/mod_Poisson.py
@@ -1,133 +1,190 @@
-import jax
-import jax.numpy as jnp
-from jax import lax
-from functools import partial
+import numpy as np
+from scipy.fftpack import dct, idct, dst, idst
+from scipy.linalg import solve_banded
+import matplotlib.pyplot as plt
 
 
-@jax.jit
-def Laplacian2D(A1, A2, A3, A4, p):
-  # p[nz, ny, nx]
-  return A1 * p[1:-1,:-2] + A2 * p[1:-1,2:] + \
-         A3 * p[:-2,1:-1] + A4 * p[2:,1:-1] + \
-         p[1:-1,1:-1]
+class Poisson:
+  def __init__(self, f, x, y=None, z=None):
+    self.f  = f
+    self.x  = x
+    self.y  = y
+    self.z  = z
+    self.dx = -x[0] + x[1]
+    self.nx = len(x)
+    if self.nx != f.shape[-1]:
+      raise Exception('Invalid length')
+    if self.y is not None:
+      self.dy = -y[0] + y[1]
+      self.ny = len(y)
+      if self.ny != f.shape[-2]:
+        raise Exception('Invalid length')
+    if self.z is not None:
+      self.dz = -z[0] + z[1]
+      self.nz = len(z)
+      if self.nz != f.shape[-3]:
+        raise Exception('Invalid length')
 
 
-@jax.jit
-def Laplacian3D(A1, A2, A3, A4, A5, A6, p):
-  # p[nz, ny, nx]
-  return A1 * p[1:-1,1:-1,:-2] + A2 * p[1:-1,1:-1,2:] + \
-         A3 * p[1:-1,:-2,1:-1] + A4 * p[1:-1,2:,1:-1] + \
-         A5 * (p[:-2,1:-1,1:-1] + p[2:,1:-1,1:-1]) + \
-         p[1:-1,1:-1,1:-1]
+  def Dirichlet(self, p1, p2, f_hat=None, my=0.e0):
+    if f_hat is None:
+      f = self.f
+    else:
+      f = f_hat
+    upper =         np.ones(self.nx) / self.dx**2
+    diag  = -2.e0 * np.ones(self.nx) / self.dx**2 + my
+    lower =         np.ones(self.nx) / self.dx**2
+    upper[0]  = 0.e0
+    lower[-1] = 0.e0
+    diag[0]   = -3.e0 / self.dx**2
+    diag[-1]  = -3.e0 / self.dx**2
+    f[0]      = f[0]  - 2.e0 * p1 / self.dx**2
+    f[-1]     = f[-1] - 2.e0 * p2 / self.dx**2
+    ab = np.stack([upper, diag, lower])
+    return solve_banded((1,1), ab, f)
 
 
-@jax.jit
-def safe_divide(numerator, denominator):
-  safe_denom = jnp.where(denominator < 1e-30, 1.e0, denominator)
-  result = numerator / safe_denom
-  return jnp.where(denominator < 1e-30, 0.e0, result)
+  def Neumann(self, dp1, dp2, f_hat=None, my=0.e0):
+    if f_hat is None:
+      f = self.f
+    else:
+      f = f_hat
+    f[0]  += dp1 / self.dx
+    f[-1] -= dp2 / self.dx
+    fhat = dct(f, norm='ortho')
+    k  = np.linspace(0.e0, self.nx-1, self.nx)
+    mx = 2.e0 * (np.cos(np.pi * k / self.nx) - 1.e0) / self.dx**2
+    p_hat     = np.zeros_like(fhat)
+    p_hat[1:] = fhat[1:] / (mx[1:] + my)
+    p_hat[0]  = 0.e0
+    return idct(p_hat, norm='ortho')
 
 
-@partial(jax.jit, static_argnums=(4, 5))
-def BiCGStab2D(p, f, dx, dy, err_tol, set_bc):
-  Ax = 0.5e0 * (dx[:-1] + dx[1:]) * dx[:-1] * dx[1:]
-  Ay = 0.5e0 * (dy[:-1] + dy[1:]) * dy[:-1] * dy[1:]
-  A  = - (dx[None,:-1] + dx[None,1]) / Ax[None,:] \
-       - (dy[:-1,None] + dy[1,None]) / Ay[:,None]
-  A1 = dx[None,1:]  / (A * Ax[None,:]) 
-  A2 = dx[None,:-1] / (A * Ax[None,:])
-  A3 = dy[1:,None]  / (A * Ay[:,None])
-  A4 = dy[:-1,None] / (A * Ay[:,None])
-  B  = f[1:-1,1:-1] / A
-
-  r0 = jnp.zeros_like(p)
-  r0 = r0.at[1:-1,1:-1].set(B - Laplacian2D(A1, A2, A3, A4, p))
-
-  r = r0
-  d = r0
-  err_r = 1.e0
-
-  def condition_fun(x):
-    A1, A2, A3, A4, p, r, r0, d, err_r, err_tol = x
-    return err_r > err_tol
-
-  def body_fun(x):
-    A1, A2, A3, A4, p, r, r0, d, err_r, err_tol = x
-    Ad = jnp.zeros_like(p)
-    Ad = Ad.at[1:-1,1:-1].set(Laplacian2D(A1, A2, A3, A4, d))
-    alpha = safe_divide(jnp.sum(r0 * r), jnp.sum(r0 * Ad))
-    s     = r - alpha * Ad
-    set_bc(s)
-    As = jnp.zeros_like(p)
-    As = As.at[1:-1,1:-1].set(Laplacian2D(A1, A2, A3, A4, s))
-    w     = safe_divide(jnp.sum(As * s), jnp.sum(As * As))
-    dp    = alpha * d + w * s
-    p    += dp
-    set_bc(p)
-    rs    = r
-    r     = s - w * As
-    beta  = alpha * safe_divide(jnp.sum(r0 * r), w * jnp.sum(r0 * rs))
-    d     = r + beta * (d - w * Ad)
-    set_bc(d)
-    err_r = jnp.sqrt(jnp.sum(dp**2) / jnp.sum(p**2))
-    return (A1, A2, A3, A4, p, r, r0, d, err_r, err_tol)
-
-  x0 = (A1, A2, A3, A4, p, r, r0, d, err_r, err_tol)
-  x  = lax.while_loop(condition_fun, body_fun, x0)
-  A1, A2, A3, A4, p, r, r0, d, err_r, err_tol = x
-  return p, err_r
+  def Dirichlet_Neumann(self, dp1, p2, f_hat=None, my=0.e0):
+    if f_hat is None:
+      f = self.f
+    else:
+      f = f_hat
+    f[0]  += dp1 / self.dx
+    f[-1] -= 2.e0 * p2 / self.dx**2
+    fhat = dct(f, type=4, norm='ortho')
+    k  = np.linspace(0.e0, self.nx-1, self.nx) + 0.5e0
+    mx = 2.e0 * (np.cos(np.pi * k / self.nx) - 1.e0) / self.dx**2
+    p_hat = fhat / (mx + my)
+    return idct(p_hat, type=4, norm='ortho')
 
 
-@partial(jax.jit, static_argnums=(5, 6))
-def BiCGStab3D(p, f, dx, dy, dz, err_tol, set_bc):
-  Ax = 0.5e0 * (dx[:-1] + dx[1:]) * dx[:-1] * dx[1:]
-  Ay = 0.5e0 * (dy[:-1] + dy[1:]) * dy[:-1] * dy[1:]
-  Az = 0.5e0 * (dz[:-1] + dz[1:]) * dz[:-1] * dz[1:]
-  A  = - (dx[None,None,:-1] + dx[None,None,1]) / Ax[None,None,:] \
-       - (dy[None,:-1,None] + dy[None,1,None]) / Ay[None,:,None] \
-       - (dz[:-1,None,None] + dz[1,None,None]) / Az[:,None,None]
-  A1 = dx[None,None,1:]  / (A * Ax[None,None,:]) 
-  A2 = dx[None,None,:-1] / (A * Ax[None,None,:])
-  A3 = dy[None,1:,None]  / (A * Ay[None,:,None])
-  A4 = dy[None,:-1,None] / (A * Ay[None,:,None])
-  A5 = dz[1:,None,None]  / (A * Az[:,None,None])
-  A6 = dz[:-1,None,None] / (A * Az[:,None,None])
-  B  = f[1:-1,1:-1,1:-1] / A
+  # y Dirichlet
+  def __y_Dirichlet(self, f, v1, v2, py1, py2, Poisson_1D, mz=0.e0):
+    f[0,:]  -= 2.e0 * py1 / self.dy**2
+    f[-1,:] -= 2.e0 * py2 / self.dy**2
+    f_hat   = dst(f,  type=2, norm='ortho', axis=0)
+    v1_hat  = dst(v1, type=2, norm='ortho')
+    v2_hat  = dst(v2, type=2, norm='ortho')
+    p_hat   = np.zeros_like(f_hat)
+    for k in range(self.ny):
+      my = 2.e0 * (np.cos(np.pi * (k+1) / self.ny) - 1.e0) / self.dy**2 + mz
+      p_hat[k,:] = Poisson_1D(v1[k], v2[k], f_hat[k,:], my)
+    return dst(p_hat, type=3, axis=0, norm='ortho')
 
-  r0 = jnp.zeros_like(p)
-  r0 = r0.at[1:-1,1:-1,1:-1].set(B - Laplacian3D(A1, A2, A3, A4, A5, A6, p))
 
-  r = r0
-  d = r0
-  err_r = 1.e0
+  # y Neumann
+  def __y_Neumann(self, f, v1, v2, dpy1, dpy2, Poisson_1D, mz=0.e0):
+    f[0,:]  += dpy1 / self.dy
+    f[-1,:] -= dpy2 / self.dy
+    f_hat   = dct(f, axis=0, norm='ortho')
+    v1_hat  = dct(v1, norm='ortho')
+    v2_hat  = dct(v2, norm='ortho')
+    p_hat   = np.zeros_like(f_hat)
+    for k in range(self.ny):
+      my = 2.e0 * (np.cos(np.pi * k / self.ny) - 1.e0) / self.dy**2 + mz
+      p_hat[k,:] = Poisson_1D(v1[k], v2[k], f_hat[k,:], my)
+    return idct(p_hat, axis=0, norm='ortho')
+  
 
-  def condition_fun(x):
-    A1, A2, A3, A4, A5, A6, p, r, r0, d, err_r, err_tol = x
-    return err_r > err_tol
+  @staticmethod
+  def __z_periodic(f, nz, dz, v1, v2, v3, v4, Poisson_2D):
+    f_hat  = np.fft.fft(f,  axis=0)
+    v1_hat = np.fft.fft(v1, axis=0)
+    v2_hat = np.fft.fft(v2, axis=0)
+    v3_hat = np.fft.fft(v3, axis=0)
+    v4_hat = np.fft.fft(v4, axis=0)
+    p_hat  = np.zeros_like(f_hat)
+    for k in range(nz):
+      mw = -2.e0 * (1.e0 - np.cos(2.e0 * np.pi * k / nz)) / dz**2
+      p_hat[k,:,:] = Poisson_2D(v1_hat[k,:], v2_hat[k,:], v3_hat[k,:], v4_hat[k,:], f_hat[k,:,:], mw)
+    return np.real(np.fft.ifft(p_hat, axis=0))
 
-  def body_fun(x):
-    A1, A2, A3, A4, A5, A6, p, r, r0, d, err_r, err_tol = x
-    Ad = jnp.zeros_like(p)
-    Ad = Ad.at[1:-1,1:-1,1:-1].set(Laplacian3D(A1, A2, A3, A4, A5, A6, d))
-    alpha = jnp.sum(r0 * r) / jnp.sum(r0 * Ad)
-    s     = r - alpha * Ad
-    set_bc(s)
-    As = jnp.zeros_like(p)
-    As = As.at[1:-1,1:-1,1:-1].set(Laplacian3D(A1, A2, A3, A4, A5, A6, s))
-    w     = jnp.sum(As * s) / jnp.sum(As * As)
-    dp    = alpha * d + w * s
-    p    += dp
-    set_bc(p)
-    rs    = r
-    r     = s - w * As
-    beta  = alpha * jnp.sum(r0 * r) / (w * jnp.sum(r0 * rs))
-    d     = r + beta * (d - w * Ad)
-    set_bc(d)
-    err_r = jnp.sqrt(jnp.sum(dp**2) / jnp.sum(p**2))
-    return (A1, A2, A3, A4, A5, A6, p, r, r0, d, err_r, err_tol)
 
-  x0 = (A1, A2, A3, A4, A5, A6, p, r, r0, d, err_r, err_tol)
-  x = lax.while_loop(condition_fun, body_fun, x0)
-  A1, A2, A3, A4, A5, A6, p, r, r0, d, err_r, err_tol = x
-  return p, err_r
+  # 2D Poisson equation
+  def x_Neumann_y_Dirichlet(self, v1, v2, v3, v4, f_hat=None, mw=None):
+    if f_hat is None and mw is None:
+      if len(v1) == self.ny and len(v2) == self.ny and len(v3) == self.nx and len(v4) == self.nx:
+        return self.__y_Dirichlet(self.f, v1, v2, v3, v4, self.Neumann)
+      else:
+        raise Exception('Invalid length')
+    elif f_hat is not None and mw is not None:
+      return self.__y_Dirichlet(f_hat, v1, v2, v3, v4, self.Neumann, mw)
+    else:
+      raise Exception('Invalid arguments')
+
+
+  def x_Dirichlet_y_Dirichlet(self, v1, v2, v3, v4, f_hat=None, mw=None):
+    if f_hat is None and mw is None:
+      if len(v1) == self.ny and len(v2) == self.ny and len(v3) == self.nx and len(v4) == self.nx:
+        return self.__y_Dirichlet(self.f, v1, v2, v3, v4, self.Dirichlet)
+      else:
+        raise Exception('Invalid length')
+    elif f_hat is not None and mw is not None:
+      return self.__y_Dirichlet(f_hat, v1, v2, v3, v4, self.Dirichlet, mw)
+    else:
+      raise Exception('Invalid arguments')
+
+
+  def x_Neumann_y_Neumann(self, v1, v2, v3, v4, f_hat=None, mw=None):
+    if f_hat is None and mw is None:
+      if len(v1) == self.ny and len(v2) == self.ny and len(v3) == self.nx and len(v4) == self.nx:
+        return self.__y_Neumann(self.f, v1, v2, v3, v4, self.Neumann)
+      else:
+        raise Exception('Invalid length')
+    elif f_hat is not None and mw is not None:
+      return self.__y_Neumann(f_hat, v1, v2, v3, v4, self.Neumann, mw)
+    else:
+      raise Exception('Invalid arguments')
+
+
+  def x_Dirichlet_y_Neumann(self, v1, v2, v3, v4, f_hat=None, mw=None):
+    if f_hat is None and mw is None:
+      if len(v1) == self.ny and len(v2) == self.ny and len(v3) == self.nx and len(v4) == self.nx:
+        return self.__y_Neumann(self.f, v1, v2, v3, v4, self.Dirichlet)
+      else:
+        raise Exception('Invalid length')
+    elif f_hat is not None and mw is not None:
+      return self.__y_Neumann(f_hat, v1, v2, v3, v4, self.Dirichlet, mw)
+    else:
+      raise Exception('Invalid arguments')
+
+
+  # 3D Poisson equation z periodic
+  def x_Neumann_y_Dirichlet_z_periodic(self, dp1x, dp2x, p1y, p2y):
+    return self.__z_periodic(self.f, self.nz, self.dz, dp1x, dp2x, p1y, p2y, self.x_Neumann_y_Dirichlet)
+
+
+  def x_Dirichlet_y_Dirichlet_z_periodic(self, dp1x, dp2x, p1y, p2y):
+    return self.__z_periodic(self.f, self.nz, self.dz, dp1x, dp2x, p1y, p2y, self.x_Dirichlet_y_Dirichlet)
+
+  def x_Neumann_y_Neumann_z_periodic(self, dp1x, dp2x, dp1y, dp2y):
+    if np.shape(dp1x) == np.shape(self.f[:,:,0]) and np.shape(dp2x) == np.shape(self.f[:,:,0]) \
+      and np.shape(dp1y) == np.shape(self.f[:,0,:]) and np.shape(dp2y) == np.shape(self.f[:,0,:]):
+      return self.__z_periodic(self.f, self.nz, self.dz, dp1x, dp2x, dp1y, dp2y, self.x_Neumann_y_Neumann)
+    else:
+      raise Exception('Invalid shape')
+
+
+  def x_Dirichlet_y_Neumann_z_periodic(self, p1x, p2x, dp1y, dp2y):
+    if np.shape(p1x) == np.shape(self.f[:,:,0]) and np.shape(p2x) == np.shape(self.f[:,:,0]) \
+      and np.shape(dp1y) == np.shape(self.f[:,0,:]) and np.shape(dp2y) == np.shape(self.f[:,0,:]):
+      return self.__z_periodic(self.f, self.nz, self.dz, p1x, p2x, dp1y, dp2y, self.x_Dirichlet_y_Neumann)
+    else:
+      raise Exception('Invalid shape')
 
