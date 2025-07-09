67c5a3a08d668ba23a1aa9aa06b02fabe42d0261
htymjun <jhatayama@s.okayama-u.ac.jp>
2025-07-09 16:00:13 +0900

    This version is faster

--- src/main.f90
+++ src/main.f90
@@ -73,11 +73,12 @@ program main
       print *, "calculation time:", m, " [min] ", s, " [sec]"
     endif
     ! save data
-    do l = 1, nz
-      do j = 1, ny
-        do i = 1, nx
-          Q(i,j,l,:) = Jacobian(j) * Q(i,j,l,:)
-    enddo;enddo;enddo
+    do m = 1, 5
+      do l = 1, nz
+        do j = 1, ny
+          do i = 1, nx
+            Q(i,j,l,m) = Jacobian(j) * Q(i,j,l,m)
+    enddo;enddo;enddo;enddo
     call cpu_time(t_start)
     write(filename, "(a, i5.5, a)") "recal/Q", int(myrank/2+1), ".dat"
     open(10,file=filename,status="replace",action="write",form="unformatted",access="sequential")
