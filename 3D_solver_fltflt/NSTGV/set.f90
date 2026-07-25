907beda2c6c2e6bb343efcf85dcd4a4595469d13
htymjun <jhatayama@s.okayama-u.ac.jp>
2026-07-25 13:27:45 +0900

    SoA and real(4) buffers for viscous kernels

--- 3D_solver_fltflt/NSTGV/set.f90
+++ 3D_solver_fltflt/NSTGV/set.f90
@@ -17,7 +17,7 @@ contains
     use mod_constant, only : id_accuracy
     integer, intent(in)  :: myrank, nx, ny, nz
     real(8), intent(in)  :: x(nx), y(ny), z(nz)
-    real(8), intent(out) :: Q(nx,5,ny,nz)
+    real(8), intent(out) :: Q(nx,ny,nz,5)
     integer i, j, k, offset
     real(8) p, RHO
     if (kind(id_accuracy) == 2) then
@@ -33,27 +33,27 @@ contains
           p = p0+RHO0*(V0**2)*(cos(2.d0*x(i)/L0)+cos(2.d0*y(j)/L0))*(cos(2.d0*z(k)/L0)+2.d0)/16.d0
           RHO = p / (R * T)
           ! rho
-          Q(i,1,j,k) = RHO
+          Q(i,j,k,1) = RHO
           ! rho u
-          Q(i,2,j,k) = RHO * V0 * sin(x(i)/L0) * cos(y(j)/L0) * cos(z(k)/L0)
+          Q(i,j,k,2) = RHO * V0 * sin(x(i)/L0) * cos(y(j)/L0) * cos(z(k)/L0)
           ! rho v
-          Q(i,3,j,k) = - RHO * V0 * cos(x(i)/L0) * sin(y(j)/L0) * cos(z(k)/L0)
+          Q(i,j,k,3) = - RHO * V0 * cos(x(i)/L0) * sin(y(j)/L0) * cos(z(k)/L0)
           ! rho w
-          Q(i,4,j,k) = 0.d0
+          Q(i,j,k,4) = 0.d0
           ! p / (gamma - 1) + 0.5 * (rhou ** 2 + rhov ** 2 ) / rho
-          Q(i,5,j,k) = p / (gamma - 1.d0) + 0.5d0 * (Q(i,2,j,k) ** 2 + Q(i,3,j,k) ** 2) / Q(i,1,j,k)
+          Q(i,j,k,5) = p / (gamma - 1.d0) + 0.5d0 * (Q(i,j,k,2) ** 2 + Q(i,j,k,3) ** 2) / Q(i,j,k,1)
     enddo;enddo;enddo
     call set_bc_cyclic(id_accuracy, nx, ny, nz, Q)
   end subroutine set_init
 
 
-  subroutine set_bc(myrank, nx, ny, nz, Jacobian, Q, Qre)
+  subroutine set_bc(myrank, nx, ny, nz, Jacobian, Q_1, Q_2, Q_3, Q_4, Q_5, Qre_1, Qre_2, Qre_3, Qre_4, Qre_5)
     use mod_constant, only : id_accuracy
     integer, intent(in), value     :: myrank, nx, ny, nz
     real(8), intent(in), device    :: Jacobian(nx,ny,nz)
-    real(8), intent(inout), device :: Q(nx,5,ny,nz)
-    real(8), intent(in), device, optional :: Qre(ny*(nz-6)*5)
-    call set_bc_cyclic(id_accuracy, nx, ny, nz, Q)
+    real(8), intent(inout), device :: Q_1(nx,ny,nz), Q_2(nx,ny,nz), Q_3(nx,ny,nz), Q_4(nx,ny,nz), Q_5(nx,ny,nz)
+    real(8), intent(in), device, optional :: Qre_1(ny*(nz-6)), Qre_2(ny*(nz-6)), Qre_3(ny*(nz-6)), Qre_4(ny*(nz-6)), Qre_5(ny*(nz-6))
+    call set_bc_cyclic(id_accuracy, nx, ny, nz, Q_1, Q_2, Q_3, Q_4, Q_5)
   end subroutine set_bc
 
 
@@ -63,4 +63,3 @@ contains
     call set_bc_mut_common(nx,ny,nz,mut,qc2)
   end subroutine set_bc_mut
 end module set
-
