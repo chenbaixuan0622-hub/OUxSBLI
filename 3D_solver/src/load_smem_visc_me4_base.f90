345e58694d6cb7beb0056e24ceb3eac95305bd04
htymjun <jhatayama@s.okayama-u.ac.jp>
2026-07-25 12:17:47 +0900

    SoA for 3D solver

--- 3D_solver/src/load_smem_visc_me4_base.f90
+++ 3D_solver/src/load_smem_visc_me4_base.f90
@@ -9,7 +9,7 @@ contains
   #if _CUDA_ARCH_ >= 900
   !> TMA version
   attributes(device) subroutine load_smem_visc_me4_base_x(it, jt, kt, j, k, &
-                                                          nx, ny, nz, inv_dy, inv_dz, Q, u, v, w, uy, vy, uz, wz)
+                                                          nx, ny, nz, inv_dy, inv_dz, Q_2, Q_3, Q_4, u, v, w, uy, vy, uz, wz)
     integer, intent(in), value              :: it            !< local idx for x direction
     integer, intent(in), value              :: jt            !< local idx for y direction
     integer, intent(in), value              :: kt            !< local idx for z direction
@@ -20,7 +20,9 @@ contains
     integer, intent(in), value              :: nz            !< number of grid points in z direction
     real(8), intent(in), device, contiguous :: inv_dy(ny-1)  !< inverse grid spacing in y (1/dy)
     real(8), intent(in), device, contiguous :: inv_dz(nz-1)  !< inverse grid spacing in z (1/dz)
-    real(8), intent(in), device, contiguous :: Q(nx,5,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_2(nx,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_3(nx,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_4(nx,ny,nz) !< conservative variables
     integer, parameter :: io_v = 2
     integer, parameter :: sx = threadsEv%x + 2*io_v + 1
     integer, parameter :: sy = threadsEv%y
@@ -53,9 +55,9 @@ contains
     start_idx = (start_i - i_base) + offset_yz
     if (tid_linear == 1) then
       if (count > 0 .and. j <= ny .and. k <= nz) then
-        call tma_bulk_load(barrier, Q(start_i,2,j,k), u(start_idx), count)
-        call tma_bulk_load(barrier, Q(start_i,3,j,k), v(start_idx), count)
-        call tma_bulk_load(barrier, Q(start_i,4,j,k), w(start_idx), count)
+        call tma_bulk_load(barrier, Q_2(start_i,j,k), u(start_idx), count)
+        call tma_bulk_load(barrier, Q_3(start_i,j,k), v(start_idx), count)
+        call tma_bulk_load(barrier, Q_4(start_i,j,k), w(start_idx), count)
       endif
     endif
     ! traditional smem load
@@ -63,12 +65,12 @@ contains
       i = i_base + ii
       idx = ii + offset_yz
       if (1 <= i .and. i <= nx .and. 3 <= j .and. j <= ny-2 .and. k <= nz) then
-        uy(idx) = (two_third * (-Q(i,2,j-1,k) + Q(i,2,j+1,k)) - one_twelfth * (-Q(i,2,j-2,k) + Q(i,2,j+2,k))) * inv_dy(j)
-        vy(idx) = (two_third * (-Q(i,3,j-1,k) + Q(i,3,j+1,k)) - one_twelfth * (-Q(i,3,j-2,k) + Q(i,3,j+2,k))) * inv_dy(j)
+        uy(idx) = (two_third * (-Q_2(i,j-1,k) + Q_2(i,j+1,k)) - one_twelfth * (-Q_2(i,j-2,k) + Q_2(i,j+2,k))) * inv_dy(j)
+        vy(idx) = (two_third * (-Q_3(i,j-1,k) + Q_3(i,j+1,k)) - one_twelfth * (-Q_3(i,j-2,k) + Q_3(i,j+2,k))) * inv_dy(j)
       endif
       if (1 <= i .and. i <= nx .and. j <= ny .and. 3 <= k .and. k <= nz-2) then
-        uz(idx) = (two_third * (-Q(i,2,j,k-1) + Q(i,2,j,k+1)) - one_twelfth * (-Q(i,2,j,k-2) + Q(i,2,j,k+2))) * inv_dz(k)
-        wz(idx) = (two_third * (-Q(i,4,j,k-1) + Q(i,4,j,k+1)) - one_twelfth * (-Q(i,4,j,k-2) + Q(i,4,j,k+2))) * inv_dz(k)
+        uz(idx) = (two_third * (-Q_2(i,j,k-1) + Q_2(i,j,k+1)) - one_twelfth * (-Q_2(i,j,k-2) + Q_2(i,j,k+2))) * inv_dz(k)
+        wz(idx) = (two_third * (-Q_4(i,j,k-1) + Q_4(i,j,k+1)) - one_twelfth * (-Q_4(i,j,k-2) + Q_4(i,j,k+2))) * inv_dz(k)
       endif
     enddo
     ! TMA syncthreads
@@ -80,7 +82,7 @@ contains
   end subroutine load_smem_visc_me4_base_x
   #else
   attributes(device) subroutine load_smem_visc_me4_base_x(it, jt, kt, j, k, &
-                                                          nx, ny, nz, inv_dy, inv_dz, Q, u, v, w, uy, vy, uz, wz)
+                                                          nx, ny, nz, inv_dy, inv_dz, Q_2, Q_3, Q_4, u, v, w, uy, vy, uz, wz)
     integer, intent(in), value              :: it            !< local idx for x direction
     integer, intent(in), value              :: jt            !< local idx for y direction
     integer, intent(in), value              :: kt            !< local idx for z direction
@@ -91,7 +93,9 @@ contains
     integer, intent(in), value              :: nz            !< number of grid points in z direction
     real(8), intent(in), device, contiguous :: inv_dy(ny-1)  !< inverse grid spacing in y (1/dy)
     real(8), intent(in), device, contiguous :: inv_dz(nz-1)  !< inverse grid spacing in z (1/dz)
-    real(8), intent(in), device, contiguous :: Q(nx,5,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_2(nx,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_3(nx,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_4(nx,ny,nz) !< conservative variables
     integer, parameter :: io_v = 2
     integer, parameter :: sx = threadsEv%x + 2*io_v + 1
     integer, parameter :: sy = threadsEv%y
@@ -110,9 +114,9 @@ contains
       i = i_base + ii
       idx = ii + offset_yz
       if (1 <= i .and. i <= nx .and. j <= ny .and. k <= nz) then
-        call pipelineMemcpyAsync(u(idx), Q(i,2,j,k))
-        call pipelineMemcpyAsync(v(idx), Q(i,3,j,k))
-        call pipelineMemcpyAsync(w(idx), Q(i,4,j,k))
+        call pipelineMemcpyAsync(u(idx), Q_2(i,j,k))
+        call pipelineMemcpyAsync(v(idx), Q_3(i,j,k))
+        call pipelineMemcpyAsync(w(idx), Q_4(i,j,k))
       endif
     enddo
     call pipelineCommit()
@@ -120,12 +124,12 @@ contains
       i = i_base + ii
       idx = ii + offset_yz
       if (1 <= i .and. i <= nx .and. 3 <= j .and. j <= ny-2 .and. k <= nz) then
-        uy(idx) = (two_third * (-Q(i,2,j-1,k) + Q(i,2,j+1,k)) - one_twelfth * (-Q(i,2,j-2,k) + Q(i,2,j+2,k))) * inv_dy(j)
-        vy(idx) = (two_third * (-Q(i,3,j-1,k) + Q(i,3,j+1,k)) - one_twelfth * (-Q(i,3,j-2,k) + Q(i,3,j+2,k))) * inv_dy(j)
+        uy(idx) = (two_third * (-Q_2(i,j-1,k) + Q_2(i,j+1,k)) - one_twelfth * (-Q_2(i,j-2,k) + Q_2(i,j+2,k))) * inv_dy(j)
+        vy(idx) = (two_third * (-Q_3(i,j-1,k) + Q_3(i,j+1,k)) - one_twelfth * (-Q_3(i,j-2,k) + Q_3(i,j+2,k))) * inv_dy(j)
       endif
       if (1 <= i .and. i <= nx .and. j <= ny .and. 3 <= k .and. k <= nz-2) then
-        uz(idx) = (two_third * (-Q(i,2,j,k-1) + Q(i,2,j,k+1)) - one_twelfth * (-Q(i,2,j,k-2) + Q(i,2,j,k+2))) * inv_dz(k)
-        wz(idx) = (two_third * (-Q(i,4,j,k-1) + Q(i,4,j,k+1)) - one_twelfth * (-Q(i,4,j,k-2) + Q(i,4,j,k+2))) * inv_dz(k)
+        uz(idx) = (two_third * (-Q_2(i,j,k-1) + Q_2(i,j,k+1)) - one_twelfth * (-Q_2(i,j,k-2) + Q_2(i,j,k+2))) * inv_dz(k)
+        wz(idx) = (two_third * (-Q_4(i,j,k-1) + Q_4(i,j,k+1)) - one_twelfth * (-Q_4(i,j,k-2) + Q_4(i,j,k+2))) * inv_dz(k)
       endif
     enddo
     call pipelineWaitPrior(0)
@@ -135,7 +139,7 @@ contains
 
 
   attributes(device) subroutine load_smem_visc_me4_base_y(it, jt, kt, i, k, &
-                                                          nx, ny, nz, inv_dx, inv_dz, Q, u, v, w, ux, vx, vz, wz)
+                                                          nx, ny, nz, inv_dx, inv_dz, Q_2, Q_3, Q_4, u, v, w, ux, vx, vz, wz)
     integer, intent(in), value              :: it            !< local idx for x direction
     integer, intent(in), value              :: jt            !< local idx for y direction
     integer, intent(in), value              :: kt            !< local idx for z direction
@@ -146,7 +150,9 @@ contains
     integer, intent(in), value              :: nz            !< number of grid points in z direction
     real(8), intent(in), device, contiguous :: inv_dx(nx-1)  !< inverse grid spacing in x (1/dx)
     real(8), intent(in), device, contiguous :: inv_dz(nz-1)  !< inverse grid spacing in z (1/dz)
-    real(8), intent(in), device, contiguous :: Q(nx,5,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_2(nx,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_3(nx,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_4(nx,ny,nz) !< conservative variables
     integer, parameter :: io_v = 2
     integer, parameter :: sx = threadsFv%x
     integer, parameter :: sy = threadsFv%y + 2*io_v + 1
@@ -165,9 +171,9 @@ contains
       j = j_base + jj
       idx = jj + offset_xz
       if (i <= nx .and. 1 <= j .and. j <= ny .and. k <= nz) then
-        call pipelineMemcpyAsync(u(idx), Q(i,2,j,k))
-        call pipelineMemcpyAsync(v(idx), Q(i,3,j,k))
-        call pipelineMemcpyAsync(w(idx), Q(i,4,j,k))
+        call pipelineMemcpyAsync(u(idx), Q_2(i,j,k))
+        call pipelineMemcpyAsync(v(idx), Q_3(i,j,k))
+        call pipelineMemcpyAsync(w(idx), Q_4(i,j,k))
       endif
     enddo
     call pipelineCommit()
@@ -175,12 +181,12 @@ contains
       j = j_base + jj
       idx = jj + offset_xz
       if (3 <= i .and. i <= nx-2 .and. 1 <= j .and. j <= ny .and. k <= nz) then
-        ux(idx) = (two_third * (-Q(i-1,2,j,k) + Q(i+1,2,j,k)) - one_twelfth * (-Q(i-2,2,j,k) + Q(i+2,2,j,k))) * inv_dx(i)
-        vx(idx) = (two_third * (-Q(i-1,3,j,k) + Q(i+1,3,j,k)) - one_twelfth * (-Q(i-2,3,j,k) + Q(i+2,3,j,k))) * inv_dx(i)
+        ux(idx) = (two_third * (-Q_2(i-1,j,k) + Q_2(i+1,j,k)) - one_twelfth * (-Q_2(i-2,j,k) + Q_2(i+2,j,k))) * inv_dx(i)
+        vx(idx) = (two_third * (-Q_3(i-1,j,k) + Q_3(i+1,j,k)) - one_twelfth * (-Q_3(i-2,j,k) + Q_3(i+2,j,k))) * inv_dx(i)
       endif
       if (i <= nx .and. 1 <= j .and. j <= ny .and. 3 <= k .and. k <= nz-2) then
-        vz(idx) = (two_third * (-Q(i,3,j,k-1) + Q(i,3,j,k+1)) - one_twelfth * (-Q(i,3,j,k-2) + Q(i,3,j,k+2))) * inv_dz(k)
-        wz(idx) = (two_third * (-Q(i,4,j,k-1) + Q(i,4,j,k+1)) - one_twelfth * (-Q(i,4,j,k-2) + Q(i,4,j,k+2))) * inv_dz(k)
+        vz(idx) = (two_third * (-Q_3(i,j,k-1) + Q_3(i,j,k+1)) - one_twelfth * (-Q_3(i,j,k-2) + Q_3(i,j,k+2))) * inv_dz(k)
+        wz(idx) = (two_third * (-Q_4(i,j,k-1) + Q_4(i,j,k+1)) - one_twelfth * (-Q_4(i,j,k-2) + Q_4(i,j,k+2))) * inv_dz(k)
       endif
     enddo
     call pipelineWaitPrior(0)
@@ -189,7 +195,7 @@ contains
 
   
   attributes(device) subroutine load_smem_visc_me4_base_z(it, jt, kt, i, j, &
-                                                          nx, ny, nz, inv_dx, inv_dy, Q, u, v, w, ux, wx, vy, wy)
+                                                          nx, ny, nz, inv_dx, inv_dy, Q_2, Q_3, Q_4, u, v, w, ux, wx, vy, wy)
     integer, intent(in), value              :: it            !< local idx for x direction
     integer, intent(in), value              :: jt            !< local idx for y direction
     integer, intent(in), value              :: kt            !< local idx for z direction
@@ -200,7 +206,9 @@ contains
     integer, intent(in), value              :: nz            !< number of grid points in z direction
     real(8), intent(in), device, contiguous :: inv_dx(nx-1)  !< inverse grid spacing in x (1/dx)
     real(8), intent(in), device, contiguous :: inv_dy(ny-1)  !< inverse grid spacing in y (1/dy)
-    real(8), intent(in), device, contiguous :: Q(nx,5,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_2(nx,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_3(nx,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_4(nx,ny,nz) !< conservative variables
     integer, parameter :: io_v = 2
     integer, parameter :: sx = threadsGv%x
     integer, parameter :: sy = threadsGv%y
@@ -219,9 +227,9 @@ contains
       k = k_base + kk
       idx = kk + offset_xy
       if (i <= nx .and. j <= ny .and. 1 <= k .and. k <= nz) then
-        call pipelineMemcpyAsync(u(idx), Q(i,2,j,k))
-        call pipelineMemcpyAsync(v(idx), Q(i,3,j,k))
-        call pipelineMemcpyAsync(w(idx), Q(i,4,j,k))
+        call pipelineMemcpyAsync(u(idx), Q_2(i,j,k))
+        call pipelineMemcpyAsync(v(idx), Q_3(i,j,k))
+        call pipelineMemcpyAsync(w(idx), Q_4(i,j,k))
       endif
     enddo
     call pipelineCommit()
@@ -229,12 +237,12 @@ contains
       k = k_base + kk
       idx = kk + offset_xy
       if (3 <= i .and. i <= nx-2 .and. j <= ny .and. 1 <= k .and. k <= nz) then
-        ux(idx) = (two_third * (-Q(i-1,2,j,k) + Q(i+1,2,j,k)) - one_twelfth * (-Q(i-2,2,j,k) + Q(i+2,2,j,k))) * inv_dx(i)
-        wx(idx) = (two_third * (-Q(i-1,4,j,k) + Q(i+1,4,j,k)) - one_twelfth * (-Q(i-2,4,j,k) + Q(i+2,4,j,k))) * inv_dx(i)
+        ux(idx) = (two_third * (-Q_2(i-1,j,k) + Q_2(i+1,j,k)) - one_twelfth * (-Q_2(i-2,j,k) + Q_2(i+2,j,k))) * inv_dx(i)
+        wx(idx) = (two_third * (-Q_4(i-1,j,k) + Q_4(i+1,j,k)) - one_twelfth * (-Q_4(i-2,j,k) + Q_4(i+2,j,k))) * inv_dx(i)
       endif
       if (i <= nx .and. 3 <= j .and. j <= ny-2 .and. 1 <= k .and. k <= nz) then
-        vy(idx) = (two_third * (-Q(i,3,j-1,k) + Q(i,3,j+1,k)) - one_twelfth * (-Q(i,3,j-2,k) + Q(i,3,j+2,k))) * inv_dy(j)
-        wy(idx) = (two_third * (-Q(i,4,j-1,k) + Q(i,4,j+1,k)) - one_twelfth * (-Q(i,4,j-2,k) + Q(i,4,j+2,k))) * inv_dy(j)
+        vy(idx) = (two_third * (-Q_3(i,j-1,k) + Q_3(i,j+1,k)) - one_twelfth * (-Q_3(i,j-2,k) + Q_3(i,j+2,k))) * inv_dy(j)
+        wy(idx) = (two_third * (-Q_4(i,j-1,k) + Q_4(i,j+1,k)) - one_twelfth * (-Q_4(i,j-2,k) + Q_4(i,j+2,k))) * inv_dy(j)
       endif
     enddo
     call pipelineWaitPrior(0)
@@ -244,7 +252,7 @@ contains
 
   !> Like load_smem_visc4_z but k_base is shifted by k_lo-1 for koff kernel launches
   attributes(device) subroutine load_smem_visc_me4_base_z_koff(it, jt, kt, i, j, &
-                                                               nx, ny, nz, inv_dx, inv_dy, Q, u, v, w, ux, wx, vy, wy, k_lo)
+                                                               nx, ny, nz, inv_dx, inv_dy, Q_2, Q_3, Q_4, u, v, w, ux, wx, vy, wy, k_lo)
     integer, intent(in), value              :: it            !< local idx for x direction
     integer, intent(in), value              :: jt            !< local idx for y direction
     integer, intent(in), value              :: kt            !< local idx for z direction
@@ -255,7 +263,9 @@ contains
     integer, intent(in), value              :: nz            !< number of grid points in z direction
     real(8), intent(in), device, contiguous :: inv_dx(nx-1)  !< inverse grid spacing in x (1/dx)
     real(8), intent(in), device, contiguous :: inv_dy(ny-1)  !< inverse grid spacing in y (1/dy)
-    real(8), intent(in), device, contiguous :: Q(nx,5,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_2(nx,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_3(nx,ny,nz) !< conservative variables
+    real(8), intent(in), device, contiguous :: Q_4(nx,ny,nz) !< conservative variables
     integer, intent(in), value              :: k_lo          !< z-index lower bound (global, 1-based)
     integer, parameter :: io_v = 2
     integer, parameter :: sx = threadsGv%x
@@ -275,9 +285,9 @@ contains
       k = k_base + kk
       idx = kk + offset_xy
       if (i <= nx .and. j <= ny .and. 1 <= k .and. k <= nz) then
-        call pipelineMemcpyAsync(u(idx), Q(i,2,j,k))
-        call pipelineMemcpyAsync(v(idx), Q(i,3,j,k))
-        call pipelineMemcpyAsync(w(idx), Q(i,4,j,k))
+        call pipelineMemcpyAsync(u(idx), Q_2(i,j,k))
+        call pipelineMemcpyAsync(v(idx), Q_3(i,j,k))
+        call pipelineMemcpyAsync(w(idx), Q_4(i,j,k))
       endif
     enddo
     call pipelineCommit()
@@ -285,12 +295,12 @@ contains
       k = k_base + kk
       idx = kk + offset_xy
       if (3 <= i .and. i <= nx-2 .and. j <= ny .and. 1 <= k .and. k <= nz) then
-        ux(idx) = (two_third * (-Q(i-1,2,j,k) + Q(i+1,2,j,k)) - one_twelfth * (-Q(i-2,2,j,k) + Q(i+2,2,j,k))) * inv_dx(i)
-        wx(idx) = (two_third * (-Q(i-1,4,j,k) + Q(i+1,4,j,k)) - one_twelfth * (-Q(i-2,4,j,k) + Q(i+2,4,j,k))) * inv_dx(i)
+        ux(idx) = (two_third * (-Q_2(i-1,j,k) + Q_2(i+1,j,k)) - one_twelfth * (-Q_2(i-2,j,k) + Q_2(i+2,j,k))) * inv_dx(i)
+        wx(idx) = (two_third * (-Q_4(i-1,j,k) + Q_4(i+1,j,k)) - one_twelfth * (-Q_4(i-2,j,k) + Q_4(i+2,j,k))) * inv_dx(i)
       endif
       if (i <= nx .and. 3 <= j .and. j <= ny-2 .and. 1 <= k .and. k <= nz) then
-        vy(idx) = (two_third * (-Q(i,3,j-1,k) + Q(i,3,j+1,k)) - one_twelfth * (-Q(i,3,j-2,k) + Q(i,3,j+2,k))) * inv_dy(j)
-        wy(idx) = (two_third * (-Q(i,4,j-1,k) + Q(i,4,j+1,k)) - one_twelfth * (-Q(i,4,j-2,k) + Q(i,4,j+2,k))) * inv_dy(j)
+        vy(idx) = (two_third * (-Q_3(i,j-1,k) + Q_3(i,j+1,k)) - one_twelfth * (-Q_3(i,j-2,k) + Q_3(i,j+2,k))) * inv_dy(j)
+        wy(idx) = (two_third * (-Q_4(i,j-1,k) + Q_4(i,j+1,k)) - one_twelfth * (-Q_4(i,j-2,k) + Q_4(i,j+2,k))) * inv_dy(j)
       endif
     enddo
     call pipelineWaitPrior(0)
